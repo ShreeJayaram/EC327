67f055c2d65357d420a553d47f70666e92f154a7
Shreenidhi Jayaram <shreej@bu.edu>
2017-10-02 16:52:10 -0400

    Add files via upload

---
+++ cashier.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+#include "cashier.h"
+
+double tip(double total, double percent)
+
+{   
+
+	int amount=0;
+	char foodliq;
+	char yesno;
+	do ( yesno = 'y') { 
+
+	cout << "What is the amount?";
+	double amount;
+	cin >> double;
+
+	cout << "Is it food(f) or liquor(l)?";
+	char foodliq;
+	cin >> fooodliq;
+
+	cout <<"Do you have more items?";
+	char yesno;
+	cin >> yesno;                
+
+}while 
+	break;	 
+
+double split(double total, int people)
+{
+ 
+
+
+}    
